ad58120df46b3b4be5aeb74e2f4ac3d82f8286b4
apa yu <rx836@hotmail.com>
2022-03-22 08:35:42 +0800

    unshift linked list

--- node.rb
+++ node.rb
@@ -8,16 +8,16 @@ class TestLinkedList < Minitest::Test
 
   def test_push
     linked_list = LinkedList.new
-    node = Node.new(1)
-    linked_list.push(node)
-    assert linked_list.is_empty? == false
-  end
-
-  def test_pust_2
-    linked_list = LinkedList.new
     linked_list.push(1)
     linked_list.push(2)
     assert linked_list.head.next.data == 2
+  end
+
+  def test_unshift
+    linked_list = LinkedList.new
+    linked_list.unshift(1)
+    linked_list.unshift(2)
+    assert linked_list.head.data == 2
   end
 end
 
@@ -37,6 +37,16 @@ class LinkedList
         current_node = current_node.next
       end
       current_node.next = Node.new(data)
+    end
+  end
+
+  def unshift(data)
+    if is_empty?
+      @head = Node.new(data)
+    else
+      new_head = Node.new(data)
+      new_head.next = @head
+      @head = new_head
     end
   end
 end
