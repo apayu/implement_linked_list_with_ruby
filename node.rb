6ce9da7be320b1100e95031b51127b427f5ecfd8
apa yu <rx836@hotmail.com>
2022-03-22 09:03:47 +0800

    size pop shift method

--- node.rb
+++ node.rb
@@ -19,6 +19,38 @@ class TestLinkedList < Minitest::Test
     linked_list.unshift(2)
     assert linked_list.head.data == 2
   end
+
+  def size
+    linked_list = LinkedList.new
+    linked_list.push(1)
+    assert linked_list.size == 1
+    linked_list.push(2)
+    assert linked_list.size == 2
+  end
+
+  def test_pop
+    linked_list = LinkedList.new
+    linked_list.push(1)
+    linked_list.push(2)
+    last_node = linked_list.pop
+    assert last_node.data == 2
+    assert linked_list.size == 1
+    last_node = linked_list.pop
+    assert last_node.data == 1
+    assert linked_list.size == 0
+  end
+
+  def test_shift
+    linked_list = LinkedList.new
+    linked_list.unshift(1)
+    linked_list.unshift(2)
+    first_node = linked_list.shift
+    assert first_node.data == 2
+    assert linked_list.size == 1
+    # last_node = linked_list.pop
+    # assert last_node.data == 1
+    # assert linked_list.size == 0
+  end
 end
 
 class LinkedList
@@ -28,6 +60,21 @@ class LinkedList
     @head == nil
   end
 
+  def size
+    if is_empty?
+      0
+    else
+      current_node = @head
+      size = 1
+      while current_node.next != nil do
+        size += 1
+        current_node = current_node.next
+      end
+      size
+    end
+  end
+
+  # insert node and from end
   def push(data)
     if is_empty?
       @head = Node.new(data)
@@ -40,6 +87,7 @@ class LinkedList
     end
   end
 
+  # insert node and from head
   def unshift(data)
     if is_empty?
       @head = Node.new(data)
@@ -47,6 +95,38 @@ class LinkedList
       new_head = Node.new(data)
       new_head.next = @head
       @head = new_head
+    end
+  end
+
+  # remove the last node and return it
+  def pop
+    if is_empty?
+      nil
+    else
+      last_node = @head
+      current_node = @head
+      if @head.next == nil
+        @head = nil
+        return last_node
+      end
+
+      while current_node.next != nil do
+        last_node = current_node
+        current_node = current_node.next
+      end
+      last_node.next = nil
+      current_node
+    end
+  end
+
+  # remove the first node and return it
+  def shift
+    if is_empty?
+      nil
+    else
+      current_node = @head
+      @head = current_node.next
+      current_node
     end
   end
 end
